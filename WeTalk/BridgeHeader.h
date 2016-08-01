fbc20fc3a97296d9433ad859af95a93ec52abfa6
Neo He <neohe2007@gmail.com>
2016-08-01 23:25:26 +0800

    添加桥接文件

---
+++ WeTalk/BridgeHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgeHeader.h
+//  WeTalk
+//
+//  Created by heyong on 16/8/1.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#import <RongIMKit/RongIMKit.h>
+
+#endif /* BridgeHeader_h */
